a076eca156d212dceca8dac333d0ebde0f5575e9
Oleg Savelos <olegsavelos@gmail.com>
2024-05-07 18:29:03 +0300

    add custom page module.

---
+++ Gizmo.Client.UI.Modules/Pages/CustomPageModule.razor.cs
@@ -0,0 +1,17 @@
+﻿using Gizmo.UI;
+using Microsoft.AspNetCore.Components;
+
+namespace Gizmo.Client.UI.Modules.Pages
+{
+    [ModuleGuid("c8b64460-0799-4145-8f09-84471a9ade69")] //module id guid, this value identifies the module globally and should be unique
+    [PageUIModule(Title = "Custom module", Description = "Custom module description")] //module title and description
+    [ModuleDisplayOrder(0)] //display order in client module menu
+    [Route("/custom")] //module access route
+    public partial class CustomPageModule : ComponentBase
+    {
+        public CustomPageModule() 
+        {
+
+        }
+    }
+}
